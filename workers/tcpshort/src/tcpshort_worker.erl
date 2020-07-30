98a6602d933ba88a4c7fe9088fa65458a3e67fe0
spalagu <m.spalagu@gmail.com>
2020-07-30 11:51:10 +0800

    fix tcp short worker compile error

--- workers/tcpshort/src/tcpshort_worker.erl
+++ workers/tcpshort/src/tcpshort_worker.erl
@@ -2,7 +2,8 @@
 
 -export([initial_state/0,
          metrics/0,
-         connect_and_send/5]).
+         connect/4,
+         request/3]).
 
 -define(Options, [
     binary,
@@ -13,8 +14,7 @@
 -define(WaitPollTime, 100).
 
 -record(s, {
-    socket = undefined,
-    sender = undefined
+    socket = undefined
 }).
 
 metrics() ->
@@ -29,22 +29,29 @@ metrics() ->
 
 initial_state() -> #s{}.
 
-connect_and_send(State, _Meta, Host, Port, Message) ->
-  {E, Socket} = gen_tcp:connect(Host, Port, ?Options),
-  case E of
-      ok -> mzb_metrics:notify({"connect.ok", counter}, 1),
-            mzb_metrics:notify({"connect.current", counter}, 1),
-            {E, Reason} = gen_tcp:send(Socket, Message),
-            case E of
-                ok -> mzb_metrics:notify({"request.ok", counter}, 1),
-                      gen_tcp:close(Socket),
-                      mzb_metrics:notify({"connect.current", counter}, -1);
-                E -> lager:error("Request sync error: ~p", [Reason]),
-                     mzb_metrics:notify({"request.error", counter}, 1),
-                     gen_tcp:close(Socket),
-                     mzb_metrics:notify({"connect.current", counter}, -1)
-            end;
-      E -> lager:error("connect error: ~p", [Socket]),
-            mzb_metrics:notify({"request.error", counter}, 1)
+connect(State, _Meta, Host, Port) ->
+    {Result, Socket} = gen_tcp:connect(Host, Port, ?Options),
+    case Result of
+        ok -> mzb_metrics:notify({"connect.ok", counter}, 1),
+              mzb_metrics:notify({"connect.current", counter}, 1);
+        error -> lager:error("connect error: ~p", [Socket]),
+             mzb_metrics:notify({"connect.error", counter}, 1)
+    end,
+    {nil, State#s{socket = Socket}}.
+
+request(State, Meta, Message) when is_list(Message) ->
+  request(State, Meta, list_to_binary(Message));
+request(#s{socket = Socket} = State, _Meta, Message) ->
+  if Socket =/= undefined -> 
+    Result = gen_tcp:send(Socket, Message),
+    case Result of
+        ok -> mzb_metrics:notify({"request.ok", counter}, 1),
+                gen_tcp:close(Socket),
+                mzb_metrics:notify({"connect.current", counter}, -1);
+        {error, Reason} -> lager:error("Request sync error: ~p", [Reason]),
+            mzb_metrics:notify({"request.error", counter}, 1),
+            gen_tcp:close(Socket),
+            mzb_metrics:notify({"connect.current", counter}, -1)
+    end
   end,
   {nil, State}.
